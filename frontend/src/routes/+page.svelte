82de55c0b4186233776e345ed4dbc993aa174980
Julian <julianao@stud.ntnu.no>
2024-03-16 00:34:31 +0100

    Revert "feat(front): redesigned login page"
    This reverts commit 5cd4d3bbfb1fa9d1d9d22bccc4fd240b1c976ed7.

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,24 +1,13 @@
 <script>
-	import { PUBLIC_API_URL } from '$env/static/public';
-	import { goto } from '$app/navigation';
-	import { logged_in } from '$lib/stores';
-	import { Button } from 'flowbite-svelte';
-
-	export function handleLogin() {
-		location.href = `${PUBLIC_API_URL}/login`;
-	}
-
-	if (logged_in) {
-		goto('/app/overview');
-	} else {
-		goto('/');
-	}
+	import Login from '$lib/components/Login.svelte';
 </script>
 
-<main class="flex flex-col items-center gap-20 justify-center min-h-screen">
-    <img src="/logo-horizontal.svg" alt="Reflection Tool Logo" class="h-14" />
-    <img src="/papers-illustration.svg" alt="Reflection Illustration" class="h-40" />
-    <Button class="bg-teal-11 hover:bg-teal-12" size="lg" on:click={handleLogin}>
-        Log in with Feide
-    </Button>
-</main>
+<svelte:head>
+	<title>NTNU Reflection</title>
+	<meta name="description" content="Svelte app" />
+</svelte:head>
+
+<Login />
+
+<style>
+</style>
